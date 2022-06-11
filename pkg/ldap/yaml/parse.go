2da7abfcf7dc1760981988e587e85c42d3a4a9eb
Alexandre Nicolaie dit Clairville <alexandre.nicolaie@gmail.com>
2022-06-12 00:46:10 +0200

    Privatise all LDAP implementation

--- pkg/ldap/yaml/parse.go
+++ pkg/ldap/yaml/parse.go
@@ -10,29 +10,29 @@ import (
 )
 
 const (
-	// AttributePrefix prefixes LDAP attributes.
-	AttributePrefix = ".@"
-	// PropertyPrefix prefixes all internal yaLDAP properties that used internally.
-	PropertyPrefix           = ".#"
-	PropertyBindPasswordAttr = PropertyPrefix + "BindPasswordAttr"
-	PropertyAllowedDN        = PropertyPrefix + "AllowedDN"
-	PropertyDeniedDN         = PropertyPrefix + "DeniedDN"
+	// attributePrefix prefixes LDAP attributes.
+	attributePrefix = ".@"
+	// propertyPrefix prefixes all internal yaLDAP properties that used internally.
+	propertyPrefix           = ".#"
+	propertyBindPasswordAttr = propertyPrefix + "BindPasswordAttr"
+	propertyAllowedDN        = propertyPrefix + "AllowedDN"
+	propertyDeniedDN         = propertyPrefix + "DeniedDN"
 )
 
 // ParseError describes a failure occurring during the parsing of the YAML definition.
 type ParseError error
 
-func parseObject(dn string, obj map[string]interface{}, index map[string]*Object) (*Object, error) {
-	object := &Object{
+func parseObject(dn string, obj map[string]interface{}, index map[string]*object) (*object, error) {
+	object := &object{
 		dn:         dn,
-		children:   map[string]*Object{},
+		children:   map[string]*object{},
 		attributes: yaldaplib.Attributes{},
 	}
 	index[dn] = object
 
 	for key, obj := range obj {
-		if strings.HasPrefix(key, AttributePrefix) {
-			key = strings.TrimPrefix(key, AttributePrefix)
+		if strings.HasPrefix(key, attributePrefix) {
+			key = strings.TrimPrefix(key, attributePrefix)
 			values, err := parseAttributeValue(key, obj)
 			if err != nil {
 				return nil, ParseError(fmt.Errorf("failed to get attribute on %s: %w", dn, err))
@@ -41,23 +41,23 @@ func parseObject(dn string, obj map[string]interface{}, index map[string]*Object
 			continue
 		}
 
-		if strings.HasPrefix(key, PropertyPrefix) {
+		if strings.HasPrefix(key, propertyPrefix) {
 			prop, err := parseAttributeValue(key, obj)
 			if err != nil {
 				return nil, ParseError(fmt.Errorf("failed to get property on %s: %w", dn, err))
 			}
 
 			switch key {
-			case PropertyBindPasswordAttr:
+			case propertyBindPasswordAttr:
 				object.bindPasswords = prop.Values()
-			case PropertyAllowedDN:
+			case propertyAllowedDN:
 				fallthrough
-			case PropertyDeniedDN:
+			case propertyDeniedDN:
 				for _, dn := range prop.Values() {
-					object.acls = append(object.acls, objectAclRule{dn, key == PropertyAllowedDN})
+					object.acls = append(object.acls, objectAclRule{dn, key == propertyAllowedDN})
 				}
 			default:
-				return nil, ParseError(fmt.Errorf("unkown property %s on %s", strings.TrimPrefix(key, PropertyPrefix), dn))
+				return nil, ParseError(fmt.Errorf("unkown property %s on %s", strings.TrimPrefix(key, propertyPrefix), dn))
 			}
 			continue
 		}
@@ -81,19 +81,19 @@ func parseObject(dn string, obj map[string]interface{}, index map[string]*Object
 			return nil, err
 		}
 
-		object.children[key].attributes[sp[0]] = &Attribute{sp[1]}
+		object.children[key].attributes[sp[0]] = &attribute{sp[1]}
 	}
 	sort.Sort(object.acls)
 	return object, nil
 }
 
-func parseAttributeValue(key string, obj interface{}) (*Attribute, error) {
+func parseAttributeValue(key string, obj interface{}) (*attribute, error) {
 	switch value := obj.(type) {
 	case []string:
-		return (*Attribute)(&value), nil
+		return (*attribute)(&value), nil
 
 	case []interface{}:
-		var values Attribute
+		var values attribute
 
 		for idx, sval := range value {
 			switch sval := sval.(type) {
@@ -110,11 +110,11 @@ func parseAttributeValue(key string, obj interface{}) (*Attribute, error) {
 		return &values, nil
 
 	case string:
-		return &Attribute{value}, nil
+		return &attribute{value}, nil
 	case int:
-		return &Attribute{strconv.Itoa(value)}, nil
+		return &attribute{strconv.Itoa(value)}, nil
 	case bool:
-		return &Attribute{strconv.FormatBool(value)}, nil
+		return &attribute{strconv.FormatBool(value)}, nil
 
 	default:
 		return nil, ParseError(fmt.Errorf("invalid attribute type '%T' on attribute '%s'", value, key))
